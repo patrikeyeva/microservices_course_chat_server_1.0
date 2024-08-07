084157c5375b83c9550bcf77db6334ea67a6ec8c
Патрикеева Лидия <lida.patrikeyeva@inbox.ru>
2024-08-08 00:56:32 +0300

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"os"
 
-	"github.com/fatih/color"
+	"github.com/joho/godotenv"
+	"github.com/patrikeyeva/microservices_course_chat_server_1.0/internal/server"
+	"github.com/patrikeyeva/microservices_course_chat_server_1.0/pkg/chat_server_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	if errEnvLoad := godotenv.Load(); errEnvLoad != nil {
+		log.Printf("Error loading .env file: %v", errEnvLoad)
+	}
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	lis, errListen := net.Listen("tcp", fmt.Sprintf("%v:%v", os.Getenv("SERVER_HOST_GRPC"), os.Getenv("SERVER_PORT_GRPC")))
+	if errListen != nil {
+		log.Fatalf("failed to listen: %v", errListen)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat_server_v1.RegisterChatV1Server(s, &server.Server{})
+	log.Printf("server listening at %v", lis.Addr())
+
+	if errServe := s.Serve(lis); errServe != nil {
+		log.Fatalf("failed to serve: %v", errServe)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,17 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/patrikeyeva/microservices_course_chat_server_1.0
 
-go 1.20
+go 1.22
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/golang/protobuf v1.5.4
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
